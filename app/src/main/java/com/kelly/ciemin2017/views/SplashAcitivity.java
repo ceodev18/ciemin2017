d27c8f6c549f6cab84dd914ec52d692f01a1fc5a
Christian Espinoza <espinoza.c@pucp.pe>
2017-06-19 21:22:30 -0500

    pls pelos no se usan guiones en las imaganes

--- app/src/main/java/com/kelly/ciemin2017/views/SplashAcitivity.java
+++ app/src/main/java/com/kelly/ciemin2017/views/SplashAcitivity.java
@@ -4,9 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.kelly.ciemin2017.MainActivity;
 import com.kelly.ciemin2017.R;
@@ -18,12 +24,21 @@ public class SplashAcitivity extends AppCompatActivity {
     Context context=this;
     // Set the duration of the splash screen
     private static final long SPLASH_SCREEN_DELAY = 3000;
+    private  Window window=null;
 
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+    /** Called when the activity is first created. */
+    Thread splashTread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_splash_acitivity);
+        startAnimations();
+        /*setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         // Hide title bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_acitivity);
@@ -36,6 +51,53 @@ public class SplashAcitivity extends AppCompatActivity {
             }
         };
         Timer timer = new Timer();
-        timer.schedule(task, SPLASH_SCREEN_DELAY);
+        timer.schedule(task, SPLASH_SCREEN_DELAY);*/
     }
+    private void startAnimations(){
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l=(LinearLayout) findViewById(R.id.ll_splash);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.iv_splash);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(context,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+        splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (true) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                   // Intent intent = new Intent(SplashAcitivity.this,
+                   //         MainActivity.class);
+                   // intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    //startActivity(intent);
+                   // SplashAcitivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    SplashAcitivity.this.finish();
+                }
+
+            }
+        };
+        splashTread.start();
+    }
+
+
 }
